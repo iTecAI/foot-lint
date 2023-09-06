d764a9477f95c1455a65202d4951901b264ad95d
iTecAI <matteovh@gmail.com>
2023-09-06 17:35:02 -0400

    Add command groups

--- foot-lint/foot_lint/__init__.py
+++ foot-lint/foot_lint/__init__.py
@@ -2,12 +2,24 @@ import click, rich
 
 @click.group()
 @click.option("--file", "-f", "input_file", required=True, help="Input file path", type=click.Path(exists=True))
-@click.option("--verbose", "-v", is_flag=True, default=False, show_default=True, help="Display extended debug information")
+@click.option("--verbose", "-v", is_flag=True, default=False, help="Display extended debug information")
 @click.pass_context
-def root(ctx: click.Context, verbose: bool = False):
+def main(ctx: click.Context, verbose: bool = False):
     ctx.ensure_object(dict)
     ctx.obj["verbose"] = verbose
 
+@main.command(help="Finds lines that do not match the style guide")
+@click.option("--fix", is_flag=True, default=False, help="Automatically fix source file when possible")
+@click.pass_context
+def lint(ctx: click.Context, fix: bool = False):
+    pass
+
+@main.command(help="Adds document author data, headers, etc")
+@click.option("--lint/--no-lint", default=True, help="Lint source code before adding header info")
+@click.pass_context
+def pack(ctx: click.Context, fix: bool = False):
+    pass
+
 if __name__ == "__main__":
-    root()
+    main()
 
